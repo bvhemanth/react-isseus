e78f56a46810deaa34951aacf41df4de746d817b
bvhemanth <kumarhemanth413@gmail.com>
2021-05-27 10:18:36 +0530

    partial commit

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,32 +1,54 @@
 import React from 'react';
 import './Details.css';
-import { Link, useLocation, BrowserRouter as Router } from "react-router-dom";
-import { useParams } from 'react-router-dom';
-class Details extends React.Component{
-    
-    // useQuery() {
-    //     return new URLSearchParams(useLocation().search);
-    // }
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+// import { Link, useLocation, BrowserRouter as Router } from "react-router-dom";
+// import { useParams } from 'react-router-dom';
+const url="https://api.github.com/repos/rails/rails/issues/";
 
+class Details extends React.Component{
+
+    state = {
+        details:[]
+      }
+
+    id = window.location.pathname;
+   
     componentDidMount(){
-        const { userName } = this.props.match.params.id;
-        console.log(userName)
-        const queryParams = new URLSearchParams(window.location.search);
-        console.log(queryParams)
-        const id = queryParams.get('id');
-        console.log(id)
+        const gitid = this.id.split('/').pop();
+    
+        axios({
+            method: 'GET',
+            url: url+gitid,
+        }).then(response => {
+            const details = response.data;
+            this.setState({ details });
+        })
+        .catch(error => {
+            console.log(error)
+        })
     }
+
     render(){
-       
         return  (
             <div>
-                 <div> each item detials</div>
                  <div className="SplitPane">
                     <div className="SplitPane-left">
-                    
+                    <div>
+                        {this.state.details.title}  <span className="cardnumber">#{this.state.details.number}</span> 
+                        <h3>Status</h3> 
+                        {this.state.details.state}
+                        <h3>Labels</h3> 
+                        {this.state.details.state}
+                        <h3>Created</h3> 
+                        { this.state.details.created_at}
+                    </div>
                     </div>
                     <div className="SplitPane-right">
-                        
+                        <h2>Summary</h2>
+                        <div>
+                        {this.state.details.body}
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/List.js
+++ src/components/List.js
@@ -16,65 +16,42 @@ class List extends React.Component{
         list: []
       }
     componentDidMount(){
-        
         axios({
             method: 'GET',
             url: url,
         })
         .then(response => {
-          console.log(response)
           const list = response.data;
-          console.log(list);
           this.setState({ list });
         })
         .catch(error => {
-            //setLoading(false)
             console.log(error)
         })
     }
-
-    renderList =  this.state.list.map((item)=>{
-        console.log(item)
-        const labels= item.labels.map((label)=>{
-            console.log(label);
-            return (
-                <div className="label" style={{'backgroundColor': '#'+label.color}}>
-                    {label.name}
-                </div>
-            )
-        })
-        return (
-            <div className="eachIssue" key={item.number}>
-                <Link to={`/issue/${item.number}`}>
-                    <div className="item" >
-                        <div className="cardtitle">
-                            {item.title}  <span className="cardnumber">#{item.number}</span>
-                        </div>
-                   
-                    {/* -{item.user.login} */}
-                    {labels}
-                    </div>
-                    
-                </Link>                
-            </div>
-        )
-    })
     render(){
         return (
         <div>
-            { this.state.list.map(item =>  <div className="eachIssue" key={item.number}>
-                <Link to={`/issue/${item.number}`}>
-                    <div className="item" >
-                        <div className="card__title">{item.title}</div>
-                    {item.number}
-               
-                    {/* {labels} */}
-                    <div class="card__author"> - {item.user.login} </div>
-                    </div>
-                    
+            { this.state.list.map(item =>  
+             
+                <div className="eachIssue" key={item.number}>
+                    <Link to={`/issue/${item.number}`}>
+                        <div className="item" >
+                            <div className="cardtitle">{item.title}</div>
+                            #{item.number}
+                            <div>
+                                {item.labels.map((label)=>{
+                                return (
+                                    <div className="label" style={{'backgroundColor': '#'+label.color}}>
+                                        {label.name}
+                                    </div>
+                                )
+                             }) }
+                            </div>
+                            <div className="cardauthor"> - {item.user.login} </div>
+                        </div>
                 </Link>                
             </div>)}
-            {this.renderList}
+           
         </div>)
     }
 }
